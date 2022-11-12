d1eb62bd8fe0289bbf0e4b8fe3734c5b263a8062
JacobWawrzynski <jacobwawrzynski@gmail.com>
2022-11-12 19:22:38 +0100

    Add MyApplications front-end

--- BankSystem/wwwroot/js/site.js
+++ BankSystem/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function updateLoanValue(amountVal) {
+    document.getElementById('amountLoanValue').value = amountVal;
+}
+
+function updateMonthsValue(monthsVal) {
+    document.getElementById('monthsLoanValue').value = monthsVal;
+}
