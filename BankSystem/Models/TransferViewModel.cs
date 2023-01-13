a8ff8fdde75de16eb4483bc8f74ee89deec2a930
Jakub Wawrzyński <jacobwawrzynski@gmail.com>
2023-01-13 01:53:24 +0100

    Update dollar transfer view model

--- BankSystem/Models/TransferViewModel.cs
+++ BankSystem/Models/TransferViewModel.cs
@@ -6,7 +6,7 @@ namespace BankSystem.Models
     {
         public double Amount { get; set; }
         public string Title { get; set; }
-        public DateTime Date { get; } = DateTime.Now;
+        public DateTime Date { get; }
         public string BeneficiaryName { get; set; }
         public string? Address { get; set; }
         public string FromAccountNumber { get; }
