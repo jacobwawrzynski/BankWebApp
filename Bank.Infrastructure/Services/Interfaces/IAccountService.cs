1bc5f5ef877d1d0886c47f33ad2535009a509e9d
Jakub Wawrzyński <jacobwawrzynski@gmail.com>
2023-08-20 01:47:02 +0200

    BNKSIM-1 Add some service interfaces

---
+++ Bank.Infrastructure/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,18 @@
+﻿using Bank.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Infrastructure.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<IEnumerable<Account>> GetAllAsync();
+        Task<Account?> GetByIdAsync(string id);
+        Task<bool> CreateAsync(Account account);
+        Task<bool> UpdateAsync(Account? account);
+        Task<bool> DeleteAsync(string id);
+    }
+}
---
+++ Bank.Infrastructure/Services/Interfaces/IClientService.cs
@@ -0,0 +1,18 @@
+﻿using Bank.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Infrastructure.Services.Interfaces
+{
+    public interface IClientService
+    {
+        Task<IEnumerable<Client>> GetAllAsync();
+        Task<Client?> GetByIdAsync(string id);
+        Task<bool> SaveAsync(Client client);
+        Task<bool> DeleteAsync(string id);
+        Task<bool> UpdateAsync(Client? client);
+    }
+}
